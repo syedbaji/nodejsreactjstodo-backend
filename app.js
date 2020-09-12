60a17f8cd4741dc6c0396e290d978df039dc2b49
Syed Baji Shaik <syedbs@langoor.com>
2020-09-12 18:32:39 +0530

    cross origin *

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 8080;
 // for parsing application/json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 // console.log(db);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
