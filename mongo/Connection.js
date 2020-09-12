27bbdff9fb6adc8698fc6a8e6cc4ad7e4d6d2953
Syed Baji Shaik <syedbs@langoor.com>
2020-09-12 16:10:12 +0530

    connection fix

--- mongo/Connection.js
+++ mongo/Connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todoDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost/todoDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://syedbaji8:mongodb$4yed@cluster0.3cdwt.mongodb.net/todoItemsDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
